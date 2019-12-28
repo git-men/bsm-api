e4b5dbd919e7abfad9061faedd59d0b5359f8034
anson <fartlek@163.com>
2019-12-28 12:04:38 +0800

    api 权限 signal保存时机的修改
    Signed-off-by: anson <fartlek@163.com>

--- backend/api_db/signals.py
+++ backend/api_db/signals.py
@@ -1,11 +1,12 @@
 from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Api
 from . import services
 
 
-@receiver(pre_save, sender=Api, dispatch_uid='create_api_permission')
+@receiver(post_save, sender=Api, dispatch_uid='create_api_permission')
 def create_api_permission(sender, instance: Api, **kwargs):
     """创建权限数据"""
     if not instance.permission:

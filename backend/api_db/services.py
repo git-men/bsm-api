87ed9da9c6f0f75d8ee20d46984001b1e568c6ae
anson <fartlek@163.com>
2019-12-28 20:06:44 +0800

    修复保存api权限的bug
    Signed-off-by: anson <fartlek@163.com>

--- backend/api_db/services.py
+++ backend/api_db/services.py
@@ -8,9 +8,13 @@ def create_api_permission(api: Api):
     if api.permission:
         return
     ctype = ContentType.objects.filter(app_label=api.app, model=api.model).first()
-    p = Permission()
-    p.name = f'执行 api.{api.slug}'
-    p.content_type = ctype
-    p.codename = f'api.{api.slug}'
-    p.save()
+
+    codename = f'api.{api.slug}'
+    p = Permission.objects.filter(codename=codename).first()
+    if not p:
+        p = Permission()
+        p.name = f'执行 api.{api.slug}'
+        p.content_type = ctype
+        p.codename = codename
+        p.save()
     api.permission = p
--- backend/api_db/signals.py
+++ backend/api_db/signals.py
@@ -1,17 +1,21 @@
-from django.db.models.signals import pre_save
 from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 from .models import Api
 from . import services
 
 
-@receiver(post_save, sender=Api, dispatch_uid='create_api_permission')
+@receiver(pre_save, sender=Api, dispatch_uid='create_api_permission')
 def create_api_permission(sender, instance: Api, **kwargs):
-    """创建权限数据"""
+    """保存前创建权限数据"""
     if not instance.permission:
-        instance.is_staff = True
         services.create_api_permission(instance)
 
+
+@receiver(post_save, sender=Api, dispatch_uid='update_api_groups')
+def update_api_groups(sender, instance: Api, **kwargs):
+    # 有主键才能加载多对多，所以用post_save
     if set(instance.permission.group_set.all()) != set(instance.groups.all()):
         instance.permission.group_set.set(instance.groups.all())
+        instance.permission.save()
